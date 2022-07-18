b774b3279d505dd55dda9c03d3a4858c663be946
dima <nevar.dima@gmail.com>
2022-07-18 10:52:35 +0300

    stable vertion. Ghanged log trigger (log printing when signal type is ON_ERROR), fixed log output in WebMessageService.getMessageById

--- src/main/java/com/qashqade/webflux/service/MessageService.java
+++ src/main/java/com/qashqade/webflux/service/MessageService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
 
 import java.util.logging.Level;
 
@@ -23,9 +24,10 @@ public class MessageService {
     public Flux<Message> allMessages(int limit) {
         long currentTime = System.currentTimeMillis();
         return messageRepository.findAllLimitBy(PageRequest.of(0,limit))
-            .log("MessageService.allMessages", Level.INFO)
-            .doOnComplete(() -> System.out.println("webflux " + limit + " Yes! " + (System.currentTimeMillis() - currentTime)/1000.0))
-            .doOnError((e) -> System.out.println("webflux " + limit + " No! " + e.getMessage()));
+            .log("MessageService.allMessages", Level.INFO, SignalType.ON_ERROR)
+            .doOnComplete(() -> System.out.printf("%s messages have been got from postgres db in %s seconds\n", limit,
+                (System.currentTimeMillis() - currentTime)/1000.0))
+            .doOnError((e) -> System.out.printf("%s can't be obtained from postgres db!\nBecause:\n%s",limit,e.getMessage()));
 
 
     }
@@ -33,9 +35,10 @@ public class MessageService {
     public Mono<Message> getMessageById(Long id) {
         long currentTime = System.currentTimeMillis();
         return messageRepository.findById(id)
-            .log("MessageService.getMessageById", Level.INFO)
-            .doOnSuccess(m -> System.out.println("webflux id " + id + " Yes! " + (System.currentTimeMillis() - currentTime)/1000.0))
-            .doOnError((e) -> System.out.println("webflux id " + id + " No! " + e.getMessage()));
+            .log("MessageService.getMessageById", Level.INFO, SignalType.ON_ERROR)
+            .doOnSuccess(m -> System.out.printf("Message id %s has been got from postgres db in %s seconds!\n", id,
+                (System.currentTimeMillis() - currentTime)/1000.0))
+            .doOnError((e) -> System.out.printf("Message id %s can't be obtained from postgres db!\nBecause:\n%s", id, e.getMessage()));
     }
 
     public Mono<Message> saveMessage(Message message) {
